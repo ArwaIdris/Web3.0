f839d3f968bc6e5274c8a037b09a47b9a01ce8e6
ArwaIdris <122272478+ArwaIdris@users.noreply.github.com>
2023-01-14 22:47:37 +0500

    upload video5

---
+++ SolidityEssentials/Video5/ViewPure.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+contract ViewPure{
+uint256 counter;
+//view can accees state variables but cannot modify them
+function soSomething() public view returns(uint256){
+    return counter;
+}
+// pure cannot even access or modift the state variables even
+function checkPure(uint256 val) public pure retur(uint256){
+    retuen val +34;
+
+}
+
+
+
+}
